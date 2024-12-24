cb3efd7f21ec732a4d024159ed585644f0f85a80
marufahmed347 <marufahmed550741@gmail.com>
2024-12-24 21:01:00 +0600

    select done

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.querySelector('#taskInput');
+    const addTaskBtn = document.querySelector('#addTaskBtn');
+    const taskList = document.querySelector('#taskList');
+    
+  });
+
