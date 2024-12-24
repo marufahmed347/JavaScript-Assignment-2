e7d1c4708798c96b2c2af121ccf3089178c86ae9
marufahmed347 <marufahmed550741@gmail.com>
2024-12-24 21:26:12 +0600

    add task done

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskBtn = document.querySelector('#addTaskBtn');
     const taskList = document.querySelector('#taskList');
     
+        // Add task
+        addTaskBtn.addEventListener('click', () => {
+
+            if(taskInput.value == " "){
+              alert("Please Enter Your Value")
+              return;
+          }
+            const taskText = taskInput.value.trim();
+            if (taskText !== '') {
+              const taskItem = document.createElement('li');
+              taskItem.className = 'flex justify-between items-center bg-gray-100 p-3 rounded-md';
+                
+            }
+          });
+        
+          
   });
 
